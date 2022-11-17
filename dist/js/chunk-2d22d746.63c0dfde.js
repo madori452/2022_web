ae2521fd11adb728821f16a75a0d1d444b7bced4
Tracy Tung <d820430@yahoo.com.tw>
2022-11-17 10:52:13 +0800

    fix index03

---
+++ dist/js/chunk-2d22d746.63c0dfde.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,c){"use strict";c.r(t);var a=c("7a23");const e={class:"about"},s=Object(a["g"])("h1",null,"This is an about page",-1),u=[s];function o(n,t){return Object(a["u"])(),Object(a["f"])("div",e,u)}var b=c("6b0d"),d=c.n(b);const i={},r=d()(i,[["render",o]]);t["default"]=r}}]);
+//# sourceMappingURL=chunk-2d22d746.63c0dfde.js.map
